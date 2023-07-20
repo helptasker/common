44fe16c9ff629b5fcb42a9d4f192b94796c51209
Kostya Ten <kostya@yandex.ru>
2023-07-20 17:43:26 +0300

    feat: Added healthcheck handler (#10)

--- tests/app_test.py
+++ tests/app_test.py
@@ -3,14 +3,14 @@ import httpx
 from helptasker_common.logger import cxt_request_id
 
 
-async def test_app(client: httpx.AsyncClient, caplog):
+async def test_app(client: httpx.AsyncClient):
     response = await client.get(url='/', headers={'Request-ID': '12345'})
     assert response.status_code == 200
     assert response.json().get('Hello') == 'World'
     assert response.headers.get('request-id') == '12345'
 
 
-async def test_cxt_request_id(client: httpx.AsyncClient, caplog):
+async def test_cxt_request_id(client: httpx.AsyncClient):
     cxt_request_id.set('12345')
     response = await client.get(url='/?test=1')
     assert response.status_code == 200
@@ -33,7 +33,7 @@ async def test_exception(client: httpx.AsyncClient):
     assert response.status_code == 500
 
 
-async def test_cors(client: httpx.AsyncClient, caplog):
+async def test_cors(client: httpx.AsyncClient):
     response = await client.options(
         url='/',
         headers={'Origin': 'http://localhost.local/', 'Access-Control-Request-Method': 'GET'},
@@ -42,6 +42,11 @@ async def test_cors(client: httpx.AsyncClient, caplog):
     assert response.headers.get('access-control-max-age') == '300'
 
 
-async def test_trusted(client: httpx.AsyncClient, caplog):
+async def test_trusted(client: httpx.AsyncClient):
     response = await client.get(url='http://localhost2.local/')
     assert response.status_code == 400
+
+
+async def test_healthcheck(client: httpx.AsyncClient):
+    response = await client.get(url='http://localhost.local/healthcheck/')
+    assert response.status_code == 200
